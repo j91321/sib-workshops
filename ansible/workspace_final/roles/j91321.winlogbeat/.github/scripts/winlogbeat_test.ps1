f933865ffb237050294ba31e817b86501dd0c5c2
j91321 <jan.trencansky@gmail.com>
2022-02-27 15:12:17 +0100

    Remove installed roles

--- ansible/workspace_final/roles/j91321.winlogbeat/.github/scripts/winlogbeat_test.ps1
+++
@@ -1,6 +0,0 @@
-$OutputVariable = Get-Service -Displayname "*winlogbeat*" | Out-String
-if(-NOT $OutputVariable -Match "Running") {
-	Write-Output "Winlogbeat service is not running"
-	exit 1
-}
-Write-Output "Winlogbeat installed succesfully"
